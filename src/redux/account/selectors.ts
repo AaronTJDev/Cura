54671be1f4efb6e386dd321fdcafdd82ca5aa4a0
Aaron Jackson <aaronjackson@Aarons-Mac-mini.attlocal.net>
2022-03-19 21:47:31 -0700

    update: adding selectors

--- src/redux/account/selectors.ts
+++ src/redux/account/selectors.ts
@@ -1,8 +1,12 @@
 import { RootStateOrAny } from 'react-redux';
 
 export const getAccount = (state: RootStateOrAny) => {
-  return state?.account?.data
+  return state?.account?.data?.user
 };
+
+export const getIsNewUser = (state: RootStateOrAny) => {
+  return state?.account?.data?.additionalUserInfo?.isNewUser;
+}
 
 export const getIsAccountLoading = (state: RootStateOrAny) => {
   return state?.account?.loading;
